9db51ae0bde399cdcdc190b66ece78f906584fdc
cairofelipedev <cairofelipedev@gmail.com>
2021-11-07 21:09:53 -0300

    btn nav module

--- components/Navs/ButtonNav.jsx
+++ components/Navs/ButtonNav.jsx
@@ -1,14 +1,15 @@
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import Button from '@material-ui/core/Button';
+
+import styles from '../../styles/main.module.scss'
+
 export default function ButtonNavWhats() {
   return (
     <>
-      <div className="contact">
-        <div className="btn-contact">
-          <Button aria-label="Abrir Contato" href="https://api.whatsapp.com/send?phone=5586995620722" className="btn-nav">
-            <WhatsAppIcon sx={{ fontSize: 30, color:'white', }} />
-          </Button>
-        </div>
+      <div className={styles.contact}>
+        <Button aria-label="Abrir Contato" href="https://api.whatsapp.com/send?phone=5586995620722" className={styles.btnnav}>
+          <WhatsAppIcon sx={{ fontSize: 30, color: 'white', }} />
+        </Button>
       </div>
     </>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,6 @@ import Hero from '../components/Hero/HeroHome'
 import AboutHome from '../components/About/AboutHome'
 import CardsHome from '../components/Cards/CardsHome' 
 import CardsCurriculum from '../components/Cards/CardsCurriculum' 
-import CssBaseline from '@material-ui/core/CssBaseline'
-import GlobalStyles from '@material-ui/core/GlobalStyles'
 import Footer from '../components/Navs/Footer';
 import ButtonNavWhats from '../components/Navs/ButtonNav'
 
@@ -13,8 +11,6 @@ import ButtonNavWhats from '../components/Navs/ButtonNav'
 function HomePage() {
   return (
     <React.Fragment>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-      <CssBaseline />
       <AppBar />
       <ButtonNavWhats />
       <Hero />
