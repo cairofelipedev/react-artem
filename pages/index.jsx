976bfca6452ed0956f50900ef4a4bd77bd674047
cairofelipedev <cairofelipedev@gmail.com>
2021-11-08 00:12:11 -0300

    next head add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import AppBar from '../components/Navs/AppBar' 
-import Hero from '../components/Hero/HeroHome' 
+import Head from 'next/head'
+import AppBar from '../components/Navs/AppBar'
+import Hero from '../components/Hero/HeroHome'
 import AboutHome from '../components/About/AboutHome'
-import CardsHome from '../components/Cards/CardsHome' 
-import CardsCurriculum from '../components/Cards/CardsCurriculum' 
+import CardsHome from '../components/Cards/CardsHome'
+import CardsCurriculum from '../components/Cards/CardsCurriculum'
 import Footer from '../components/Navs/Footer';
 import ButtonNavWhats from '../components/Navs/ButtonNav'
 import GlobalStyles from '@material-ui/core/GlobalStyles'
@@ -12,6 +13,14 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 function HomePage() {
   return (
     <React.Fragment>
+      <Head>
+        <title>ARTEM CARDIOLOGIA INDIVIDUALIZADA</title>
+        <link rel="icon" href="/artem.ico" />
+        <meta name="description" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA - Serviço médico especializado e indiviadualizado" />
+        <meta property="og:title" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA" />
+        <meta property="og:url" content="https://react-artem.vercel.app/" />
+        <meta property="og:image" content="https://react-artem.vercel.app/_next/image?url=%2Fartem.png&w=1080&q=75" />
+      </Head>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <CssBaseline />
       <AppBar />
@@ -19,7 +28,7 @@ function HomePage() {
       <Hero />
       <CardsHome />
       <AboutHome />
-      <CardsCurriculum/>
+      <CardsCurriculum />
       <Footer />
     </React.Fragment>
   );
