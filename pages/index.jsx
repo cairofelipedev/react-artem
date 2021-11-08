1e7ec803d2b950b351febc45a0cc17b318627dc8
cairofelipe <cairo.felipe@paraiba.email>
2021-11-08 08:54:04 -0300

    New 2 og:image 4

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ function HomePage() {
         <meta name="description" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA - Serviço médico especializado e indiviadualizado" />
         <meta property="og:title" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA" />
         <meta property="og:url" content="https://react-artem.vercel.app/" />
-        <meta property="og:image" content="https://react-artem.vercel.app/_next/image?url=%2Fartem2.jpg&w=1080&q=75" />
+        <meta property="og:image" content="https://react-artem.vercel.app/_next/image?url=%2Fartem2.jpg&w=1920&q=75" />
       </Head>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <CssBaseline />
