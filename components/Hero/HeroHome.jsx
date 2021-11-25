63d0520e96a3e6a9a9019a760727f282e9d4963a
cairofelipedev <cairofelipedev@gmail.com>
2021-11-24 22:00:34 -0300

    hero fix

--- components/Hero/HeroHome.jsx
+++ components/Hero/HeroHome.jsx
@@ -7,31 +7,32 @@ import 'animate.css';
 export default function Hero() {
   return (
     <>
-      <Container className={styles.Hero} maxWidth="full" component="main" sx={{ pt: 8, pb: 6 }}>
-        <Container>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            container
-            className="animate__animated animate__fadeInUp animate__delay-0.6s"
-          >
-            <Typography
-              className={styles.titleMain}
+      <div className={styles.HeroMain}>
+        <Container className={styles.Hero} maxWidth="full" component="main" sx={{ pt: 8, pb: 6 }}>
+          <Container>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              container
+              className="animate__animated animate__fadeInUp animate__delay-0.6s"
             >
-              Cardiologia é mais
-              do que medicina...
-              é uma arte
-            </Typography>
-            <Typography
-              className={styles.titleSecondary}
-            >
-              SEJA BEM VINDO À UM ATENDIMENTO PERSONALIZADO
-            </Typography>
-          </Grid>
+              <Typography
+                className={styles.titleMain}
+              >
+                Cardiologia é mais
+                do que medicina...
+                é uma arte
+              </Typography>
+              <Typography
+                className={styles.titleSecondary}
+              >
+                SEJA BEM VINDO À UM ATENDIMENTO PERSONALIZADO
+              </Typography>
+            </Grid>
+          </Container>
         </Container>
-      </Container>
-      {/* End hero unit */}
+      </div>
     </>
   );
 }
