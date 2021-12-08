572e6092307882dc685132dee6063d43857b4e4c
cairofelipe <cairo.felipe@paraiba.email>
2021-12-08 09:39:58 -0300

    Add GA start

--- js/config.local.js
+++ js/config.local.js
@@ -1,6 +1,7 @@
 const config = {
   USERMAIL: "${{ secrets.USERMAIL }}",
-  PASSMAIL: "${{ secrets.USERPASS }}"
+  PASSMAIL: "${{ secrets.USERPASS }}",
+  NEXT_PUBLIC_GA_ID: "${{ secrets.NEXT_PUBLIC_GA_ID }}"
 }
 
 module.exports = config
