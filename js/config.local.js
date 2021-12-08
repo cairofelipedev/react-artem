40602d38b584675ef474cc83cfd1c2dc50dbdc15
cairofelipe <cairo.felipe@paraiba.email>
2021-12-08 12:00:20 -0300

    Ga 2

--- js/config.local.js
+++ js/config.local.js
@@ -1,7 +1,6 @@
 const config = {
   USERMAIL: "${{ secrets.USERMAIL }}",
-  PASSMAIL: "${{ secrets.USERPASS }}",
-  NEXT_PUBLIC_GA_ID: "${{ secrets.NEXT_PUBLIC_GA_ID }}"
+  PASSMAIL: "${{ secrets.USERPASS }}"
 }
 
 module.exports = config
