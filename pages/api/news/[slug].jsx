66fb24556bbcc4c1f6c9e88ebecba6b89922c5b6
cairofelipedev <cairofelipedev@gmail.com>
2022-01-08 23:23:26 -0300

    Fix SingleNews api

---
+++ pages/api/news/[slug].jsx
@@ -0,0 +1,6 @@
+const { news } = require("./data.json");
+
+export default (req,res) => {
+  const newsArticle = news.filter((item) => item.slug === req.query.slug);
+  res.status(200).json(newsArticle);
+}
