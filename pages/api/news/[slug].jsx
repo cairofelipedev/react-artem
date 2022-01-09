dc9873478ef79b4cbbf8175dcb961b413a04989d
cairofelipedev <cairofelipedev@gmail.com>
2022-01-08 22:12:38 -0300

    Iniciando API para produção

--- pages/api/news/[slug].jsx
+++
@@ -1,6 +0,0 @@
-const { news } = require("./data.json");
-
-export default (req,res) => {
-  const newsArticle = news.filter((item) => item.slug === req.query.slug);
-  res.status(200).json(newsArticle);
-}
