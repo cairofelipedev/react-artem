04fee51af1d9252398ea548be887ab35d040ef88
cairofelipedev <cairofelipedev@gmail.com>
2022-01-16 23:18:31 -0300

    fix container

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -3,9 +3,9 @@ import { API_URL } from '@/config/index'
 import NewsItem from '@/components/NewsItem'
 import Link from 'next/link'
 import GlobalStyles from '@material-ui/core/GlobalStyles'
-import CarouselBlog from '@/components/Carousel/CarouselBlog'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import styles from '@/styles/main.module.scss'
+import { Container } from '@material-ui/core'
 export default function News({ news }) {
 
   return (
@@ -13,11 +13,12 @@ export default function News({ news }) {
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <CssBaseline />
       <Layout>
-        <CarouselBlog />
         <div className={styles.blogPage}>
-          <h1>CONFIRA AS ÚLTIMAS
-            PUBLICAÇÕES DO BLOG
-          </h1>
+          <Container>
+            <h1>CONFIRA AS ÚLTIMAS
+              PUBLICAÇÕES DO BLOG
+            </h1>
+          </Container>
           {news.length === 0 && <h3>No News</h3>}
           {news.map((item) => (
             <NewsItem key={item.id} news={item} />
