5d786c021a7852b4ab2d5eaf1e4bcd70448a3d53
cairofelipedev <cairofelipedev@gmail.com>
2022-01-16 22:47:24 -0300

    fix prop key 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,33 @@
-import * as React from 'react';
-import Head from 'next/head'
-import AppBar from '@/components/Navs/AppBar'
-import Hero from '@/components/Hero/HeroHome'
-import AboutHome from '@/components/Contents/AboutHome'
-import CardsHome from '@/components/Cards/CardsHome'
-import FormHome from '@/components/Contents/FormContent'
-import CardsCurriculum from '@/components/Cards/CardsCurriculum'
-import Footer from '@/components/Navs/Footer';
-import Box from '@material-ui/core/Box';
-import ButtonNavWhats from '@/components/Navs/ButtonNav'
+import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
-import NewsItemHome from '@/components/NewsItemHome'
+import NewsItem from '@/components/NewsItem'
+import Link from 'next/link'
 import GlobalStyles from '@material-ui/core/GlobalStyles'
+import CarouselBlog from '@/components/Carousel/CarouselBlog'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import ContentBlog from '@/components/Contents/BlogContent';
-
-export default function Home({ news }) {
+import styles from '@/styles/main.module.scss'
+export default function News({ news }) {
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>ARTEM CARDIOLOGIA INDIVIDUALIZADA</title>
-        <link rel="icon" href="/artem.ico" />
-        <meta name="description" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA - Serviço médico especializado e indiviadualizado" />
-        <meta property="og:title" content="ARTEM CARDIOLOGIA INDIVIDUALIZADA" />
-        <meta property="og:url" content="https://www.artemcardiologia.com.br/" />
-        <meta property="og:image" content="https://www.artemcardiologia.com.br/artem2.jpg" />
-        <meta name="facebook-domain-verification" content="0mkluqfdz4uszpiq52m1f18vq607gx" />
-      </Head>
+    <div>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <CssBaseline />
-      <AppBar />
-      <ButtonNavWhats />
-      <Hero />
-      <AboutHome />
-      <CardsHome />
-      <CardsCurriculum />
-      <ContentBlog />
-      {news.length === 0 && <h3>No News</h3>}
-      {news.map((item) => (
-        <Box key={item.id}>
-          <NewsItemHome news={item} />
-        </Box>
-      ))}
-      <FormHome />
-      <Footer />
-    </React.Fragment>
+      <Layout>
+        <CarouselBlog />
+        <div className={styles.blogPage}>
+          <h1>CONFIRA AS ÚLTIMAS
+            PUBLICAÇÕES DO BLOG
+          </h1>
+          {news.length === 0 && <h3>No News</h3>}
+          {news.map((item) => (
+            <NewsItem key={item.id} news={item} />
+          ))}
+          <Link href="/">
+            <a>Go Back</a>
+          </Link>
+        </div>
+      </Layout>
+    </div>
   )
 }
 
@@ -54,7 +36,7 @@ export async function getStaticProps() {
   const news = await res.json()
 
   return {
-    props: { news: news.slice(0, 5) },
+    props: { news },
     revalidate: 1
   }
 }
